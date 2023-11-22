47e972a6a081930cb1b5e7db1b3e6109cb842756
Redin666 <51113071+Redin666@users.noreply.github.com>
2023-11-22 13:45:27 +0300

    Add files via upload

---
+++ Task 1/Test/Task 1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double formula(double x)
+{
+    const double epsilon = 1e-6;
+
+    double result = 0.0;
+    double term = 0.0;
+    int n = 1;
+
+    do {
+        term = (pow(2, n) * pow(x, 2 * n)) / (n + 1);
+        result += term;
+        n++;
+    } while (fabs(term) > epsilon);
+
+    return result;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double x;
+
+    do {
+        cout << "Введите значение x: ";
+        if ((cin >> x)) {
+            cout << "Значение ряда для x = " << x << " равно: " << formula(x) << endl;
+            break;
+        }
+        else {
+            cout << "Ошибка ввода! Пожалуйста, введите число." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    } while (!cin.fail());
+
+    return 0;
+}
